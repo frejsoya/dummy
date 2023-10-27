0c0d6582a06bce2880467a12eceef9173f358ef5
Frej Soya <frej.soya@gmail.com>
2023-10-27 10:09:17 +0200

    Cleanups.

--- bin/main.ml
+++ bin/main.ml
@@ -14,7 +14,7 @@ let event_param_of string =
   match string with
   | "start_workout" -> Start_workout
   | "complete_workout" -> Complete_workout
-  (* TODO: Quick hack as we have no phase > 3*)
+  (* Quick and easy as we we just have 3 phases *)
   | "phase_progression_1" -> Phase_progression 1
   | "phase_progression_2" -> Phase_progression 2
   | "phase_progression_3" -> Phase_progression 3
@@ -67,10 +67,15 @@ let make_new_complete last_event =
       new_event = true;
     }
   in
-  Format.eprintf "Creating new event:\n%a\n" pp_event_log new_event;
+  (* Format.eprintf "Creating new event:\n%a\n" pp_event_log new_event; *)
   new_event
 
-module FSM = struct
+(*
+
+  Small state machine with each event_log being transitions between states
+
+  *)
+module EventValidate = struct
   let rec state_init state events =
     match events with
     | [] -> state
@@ -122,11 +127,11 @@ module FSM = struct
   let start events = state_init [] events
 end
 
-let pp_list = Format.pp_print_list pp_event_log
 let clean s = String.(sub s ~pos:1 ~len:(length s - 2))
 let clean_opt = function "NULL" -> None | s -> Option.some @@ clean s
 
 let clean_ts s =
+  (* Parses a timestamp with current standard functions..*)
   let s = clean s in
   let s = s ^ "Z" in
   (* make it proper rfc3339 *)
@@ -134,71 +139,88 @@ let clean_ts s =
   | Ok t -> t
   | Error msg -> failwith @@ "Parsing " ^ s ^ " failed with " ^ msg
 
+(* Read and parse data *)
+let read_event_sql_dump file =
+  let read_data ic =
+    let lines = Seq.of_dispenser @@ fun () -> In_channel.input_line ic in
+    let filter_data line =
+      String.starts_with ~prefix:"INSERT INTO" line == false
+    in
+    let read_event_log_data line =
+      Scanf.sscanf line "(%d, %d, %s@, %s@, %s@, %s@)"
+        (fun id user_id timestamp param value session_id ->
+          let param = String.(sub param ~pos:1 ~len:(length param - 2)) in
+          let param = event_param_of param in
+          let value = clean_opt value in
+          let session_id = clean_opt session_id in
+          let timestamp, _opt, _opt2 = clean_ts timestamp in
+          let new_event = false in
+          { id; user_id; timestamp; param; value; session_id; new_event })
+    in
+
+    (* Filter events we dont care about *)
+    let whitelist_phases event =
+      match event.param with
+      | Start_workout -> true
+      | Complete_workout -> true
+      | Phase_progression _ -> true
+      | ReadTip -> true
+      | PlayVideo -> true
+      | Other _str -> false
+    in
+    let after_cutoff e =
+      let cutoff_date = Ptime.of_date (2023, 06, 01) |> Option.get in
+      Ptime.is_later e.timestamp ~than:cutoff_date
+    in
+    let whitelist e = after_cutoff e && whitelist_phases e in
+
+    let event_log_data =
+      lines |> Seq.filter filter_data
+      |> Seq.map read_event_log_data
+      |> Seq.filter whitelist
+    in
+    Format.eprintf "Reading event data \n";
+    (* lazy sequence materalized as an array*)
+    event_log_data |> Array.of_seq
+
+  in
+
+   In_channel.with_open_text file read_data
+
+
+(* Main function *)
+
 let () =
   Printexc.record_backtrace true;
 
   print_endline "Hello, World!\n";
   let file = "../event-logs" in
+  let event_log_data = read_event_sql_dump file in
 
-  (* Ineffective, loading all into mem as a linked list.  *)
-  let lines = In_channel.with_open_text file In_channel.input_lines in
+  Format.eprintf "Sorting event log by user id\n";
+  Array.sort ~cmp:(fun a b -> a.user_id - b.user_id) event_log_data;
 
-  let data =
-    List.filter
-      ~f:(fun line -> String.starts_with ~prefix:"INSERT INTO" line == false)
-      lines
+  let group_by_user =
+    Array.to_seq event_log_data |> Seq.group (fun a b -> a.user_id == b.user_id)
   in
-  let data =
-    List.map data ~f:(fun line ->
-        let elem =
-          Scanf.sscanf line "(%d, %d, %s@, %s@, %s@, %s@)"
-            (fun id user_id timestamp param value session_id ->
-              let param = String.(sub param ~pos:1 ~len:(length param - 2)) in
-              let param = event_param_of param in
-              let value = clean_opt value in
-              let session_id = clean_opt session_id in
-              let timestamp, _opt, _opt2 = clean_ts timestamp in
-              let new_event = false in
-              { id; user_id; timestamp; param; value; session_id; new_event })
-        in
-        elem)
-  in
-  (* Filter events we dont care about *)
-  let whitelist_phases event =
-    match event.param with
-    | Start_workout -> true
-    | Complete_workout -> true
-    | Phase_progression _ -> true
-    | ReadTip -> true
-    | PlayVideo -> true
-    | Other _str -> false
-  in
-  let after_cutoff e =
-    let cutoff_date = Ptime.of_date (2023, 06, 01) |> Option.get in
-    Ptime.is_later e.timestamp ~than:cutoff_date
-  in
-  let whitelist e = after_cutoff e && whitelist_phases e in
-  let data = List.filter data ~f:whitelist in
+  Format.eprintf "Verifying and adding missing events\n";
 
-  (* Sort and group by user id *)
-  let data = List.sort ~cmp:(fun a b -> a.user_id - b.user_id) data in
-  let seq = List.to_seq data in
-  let group_by_user = Seq.group (fun a b -> a.user_id == b.user_id) seq in
-
+  (* Validate and transform by users group *)
   let () =
     Seq.iter
       (fun user_group ->
         let group = List.of_seq user_group in
         let user_id = (List.hd group).user_id in
+
         let group =
           List.sort
             ~cmp:(fun a b -> Ptime.compare a.timestamp b.timestamp)
             group
         in
-        let fixed_group = FSM.start group |> List.rev in
+        let group = EventValidate.start group |> List.rev in
         Format.printf "\nGROUP_user %d\n " user_id;
         let pp = Ptime.pp_human ~frac_s:6 () in
-        List.iter fixed_group ~f:(fun e ->
+        List.iter group ~f:(fun e ->
             if e.new_event then Format.printf "NEW" else ();
 
             Format.printf "\t%a, %s,\n" pp e.timestamp
